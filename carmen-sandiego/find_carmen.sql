a98aede3ef9da9dfdbe6171442a575345179aaa7
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-08 22:09:59 -0500

    Solved Clue 6

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -23,6 +23,7 @@ SELECT * FROM COUNTRY LIMIT 1;
 -- code2          | AF
 
 
+
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 -- SQL COMMAND:
 -- SELECT population, name FROM country WHERE region = 'Southern Europe';
@@ -48,6 +49,7 @@ SELECT * FROM COUNTRY LIMIT 1;
 -- CLUE #1 ANSWER: She was seen in the Holy See (Vatican City State).
 
 
+
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 -- SQL COMMAND:
 -- SELECT countrycode, "language", isofficial, percentage FROM countrylanguage WHERE countrycode = 'VAT';
@@ -57,6 +59,7 @@ SELECT * FROM COUNTRY LIMIT 1;
 --  VAT         | Italian  | t          |          0
 -- (1 row)
 -- CLUE #2 ANSWER: The official spoken language is Italian.
+
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
@@ -73,6 +76,7 @@ SELECT * FROM COUNTRY LIMIT 1;
 -- CLUE #3 ANSWER: The country of 'San Marino' in 'Southern Europe' speaks only Italian.
 
 
+
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 -- SQL COMMAND:
 -- SELECT * FROM city WHERE countrycode = 'SMR' ORDER BY population ASC;
@@ -83,6 +87,7 @@ SELECT * FROM COUNTRY LIMIT 1;
 --  3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 -- (2 rows)
 -- CLUE #4 ANSWER: The other city that Carmen Sandiego could be flying to is 'Serravalle' in the country of 'San Marino' in 'Southern Europe'.  
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
@@ -106,10 +111,27 @@ SELECT * FROM COUNTRY LIMIT 1;
 -- CLUE #5 ANSWER (PART 2): The country is 'Brazil'. 
 
 
+
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
+-- SQL COMMAND: 
+-- SELECT capital FROM country WHERE code = 'BRA';
+-- QUERY RESULTS: 
+-- capital 
+---------
+--  211 
+-- SQL COMMAND:
+-- SELECT * FROM city WHERE id = 211;
+-- QUERY RESULTS:
+--  id  |    name    | countrycode |     district     | population 
+-----+------------+-------------+------------------+------------
+--  211 | Brasï¿½lia | BRA         | Distrito Federal |    1969868
+-- (1 row)
+-- CLUE #6 ANSWER: Carmen Sandiego is headed towards 'Brasilia', the capital of Brazil located in the 'Distrito Federal'.  
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
+-- SQL COMMAND: 
+-- 
 
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
