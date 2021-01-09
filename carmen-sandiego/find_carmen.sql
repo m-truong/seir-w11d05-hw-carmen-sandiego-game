3d6e861159d50fa45ec700b622dd6d96e181adf1
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-08 21:18:10 -0500

    Solved Clue 2 and 3
    The official spoken language at the Holy See is Italian. 'San Marino' in 'Southern Europe' is a nearby country that has a 100 percentage of the population speaking only Italian.

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -48,14 +48,28 @@ SELECT * FROM COUNTRY LIMIT 1;
 -- CLUE #1 ANSWER: She was seen in the Holy See (Vatican City State).
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
-
-
+-- SQL COMMAND:
+-- SELECT countrycode, "language", isofficial, percentage FROM countrylanguage WHERE countrycode = 'VAT';
+-- QUERY RESULTS: 
+-- countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+--  VAT         | Italian  | t          |          0
+-- (1 row)
+-- CLUE #2 ANSWER: The official spoken language is Italian.
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
-
-
-
+-- SQL COMMAND:
+-- SELECT countrycode, "language", isofficial, percentage FROM countrylanguage WHERE isofficial = true AND language = 'Italian';
+-- QUERY RESULTS: 
+-- countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+--  ITA         | Italian  | t          |       94.1
+--  SMR         | Italian  | t          |        100
+--  VAT         | Italian  | t          |          0
+--  CHE         | Italian  | t          |        7.7
+-- (4 rows)
+-- CLUE #3 ANSWER: The country of 'San Marino' in 'Southern Europe' speaks only Italian.
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
